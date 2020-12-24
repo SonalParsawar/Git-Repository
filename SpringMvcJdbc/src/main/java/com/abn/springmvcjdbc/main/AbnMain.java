e78d1746de5841e2f2ac79a1ec88caeafc8d20c1
Sonal Parsawar <sonalparsawar210@gmail.com>
2020-12-24 19:59:36 +0530

    SpringMvcJdbc

---
+++ SpringMvcJdbc/src/main/java/com/abn/springmvcjdbc/main/AbnMain.java
@@ -0,0 +1,8 @@
+package com.abn.springmvcjdbc.main;
+
+public class AbnMain {
+
+	public static void main(String[] args) {
+		System.out.println("Hello Sonal");
+	}
+}
---
+++ SpringMvcJdbc/src/test/java/com/abn/springmvcjdbc/main/test/AbnMainTest.java
@@ -0,0 +1,12 @@
+package com.abn.springmvcjdbc.main.test;
+
+import org.testng.annotations.Test;
+
+public class AbnMainTest {
+	
+	@Test
+	public void test() {
+		System.out.println("Hello Test!");
+	}
+
+}
